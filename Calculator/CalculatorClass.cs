0ee3fbb5726a6c2a0ff525591e1589bbd507c7e0
Gaurav Mori <gkmori444@gmail.com>
2023-04-03 15:05:54 +0530

    Practical-1
    Inside Feature Branch

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+      class CalculatorClass
+    {
+
+        public static void Main (String[] args)
+        {
+            //int num1, num2;
+            Console.WriteLine("Enter First Number : ");
+             int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Enter Second Number : ");
+            int.TryParse(Console.ReadLine(), out int num2);
+
+            Console.WriteLine("OUTPUT");
+            Console.WriteLine($"{num1} + {num2} :" + (num1 + num2));
+            Console.WriteLine($"{num1} - {num2} :" + (num1 - num2));
+            Console.WriteLine($"{num1} + {num2} :" + (num1 * num2));
+            Console.WriteLine($"{num1} / {num2} :" + (num1 / num2));
+
+
+
+        }
+    }
+}
